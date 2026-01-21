7e9e1442623a725278a99f8b4ca76bf13398be36
H3x1k <hudsonconway612@gmail.com>
2026-01-21 12:41:34 -0600

    Finish lab00

--- lab00/src/main.c
+++ lab00/src/main.c
@@ -4,8 +4,13 @@ int main(void) {
     printf("CS2413 Lab00 Starter Demo\n");
 
     // TODO: Replace the placeholder information below
+<<<<<<< HEAD
     printf("Name: Shawn\n");
     printf("R#: 11714613\n");
+=======
+    printf("Name: Hudson Conway\n");
+    printf("R#: ****45550\n");
+>>>>>>> ac2ab4d (Finish lab00)
     printf("Section: 002\n");
     printf("Message: I can use GitHub for CS2413 labs!\n");
 

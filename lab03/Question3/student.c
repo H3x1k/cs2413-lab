3c8d7b4cc90ba0459be304b0a9bc765e41d4545b
H3x1k <hudsonconway612@gmail.com>
2026-02-24 20:08:39 -0600

    Finish Lab 3

--- lab03/Question3/student.c
+++ lab03/Question3/student.c
@@ -14,5 +14,19 @@
 
 bool hasCycle(struct ListNode *head) {
     // TODO: implement
+    if (head == NULL) return false;
 
+    struct ListNode* slowNode = head;
+    struct ListNode* fastNode = head->next;
+
+    while (slowNode != NULL && fastNode != NULL) {
+        if (slowNode == fastNode) return true;
+        slowNode = slowNode->next;
+        if (fastNode->next != NULL)
+            fastNode = fastNode->next->next;
+        else
+            return false;
+    }
+
+    return false;
 }
--- lab03/Question4/student.c
+++ lab03/Question4/student.c
@@ -14,5 +14,21 @@
 
 int middleNodeValue(struct ListNode *head) {
     // TODO: implement
+    if (head == NULL) return 0;
+    if (head->next == NULL) return head->val;
+
+    struct ListNode* slowNode = head;
+    struct ListNode* fastNode = head->next;
+
+    while (fastNode->next != NULL) {
+        slowNode = slowNode->next;
+        fastNode = fastNode->next->next;
+
+        if (fastNode == NULL) {
+            return slowNode->val;
+        }
+    }
+
+    return slowNode->next->val;
 
 }
